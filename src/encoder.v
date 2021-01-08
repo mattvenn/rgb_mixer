d9b9609917cff6ce4b42fcdf8636009a5298df54
Matt Venn <matt@mattvenn.net>
2021-01-08 15:12:53 +0100

    blank modules

---
+++ src/encoder.v
@@ -0,0 +1,11 @@
+`default_nettype none
+`timescale 1ns/1ns
+module encoder (
+    input clk,
+    input reset,
+    input a,
+    input b,
+    output reg [7:0] value
+);
+
+endmodule
---
+++ src/top.v
@@ -0,0 +1,17 @@
+`default_nettype none
+`timescale 1ns/1ns
+module top (
+    input clk12,
+    input reset_n,
+    input enc0_a,
+    input enc0_b,
+    input enc1_a,
+    input enc1_b,
+    input enc2_a,
+    input enc2_b,
+    output pwm0_out,
+    output pwm1_out,
+    output pwm2_out
+);
+
+endmodule
