60079a7753002fec6c4c59b35ecbe7dfc7a3ba1e
Matt Venn <matt@mattvenn.net>
2021-01-12 15:27:17 +0100

    remove code

--- src/debounce.v
+++ src/debounce.v
@@ -1,31 +1,10 @@
 `default_nettype none
 `timescale 1ns/1ns
-module debounce #(
-    parameter HIST_LEN = 8
-)(
+module debounce (
     input wire clk,
     input wire reset,
     input wire button,
     output reg debounced
 );
-    localparam on_value = 2 ** HIST_LEN - 1;
-    reg [HIST_LEN-1:0] button_hist;
-
-    always @(posedge clk) begin
-        if(reset) begin
-
-            button_hist <= 0;
-            debounced <= 0;
-
-        end else begin
-
-            button_hist = {button_hist[HIST_LEN-2:0], button };
-
-            if(button_hist == on_value) 
-               debounced <= 1;
-            else if(button_hist == 0)
-               debounced <= 0;
-       end
-    end
 
 endmodule
