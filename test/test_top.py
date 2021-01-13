5cc92927d1370793d9036b47070921fe26ab0c80
matt venn <matt@mattvenn.net>
2021-01-13 20:58:16 +0100

    take new test from main

--- test/test_top.py
+++ test/test_top.py
@@ -4,6 +4,8 @@ from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
 import random
 from test.test_encoder import Encoder
 
+clocks_per_phase = 10
+
 async def reset(dut):
     dut.enc0_a <= 0
     dut.enc0_b <= 0
@@ -17,10 +19,20 @@ async def reset(dut):
     dut.reset <= 0;
     await ClockCycles(dut.clk, 5) # how long to wait for the debouncers to clear
 
+async def run_encoder_test(encoder, dut_enc):
+    max_count = 255
+    for i in range(clocks_per_phase * 2 * max_count):
+        await encoder.update(1)
+
+    # let noisy transition finish, otherwise can get an extra count
+    for i in range(10):
+        await encoder.update(0)
+    
+    assert(dut_enc == max_count)
+
 @cocotb.test()
 async def test_all(dut):
     clock = Clock(dut.clk, 10, units="us")
-    clocks_per_phase = 10
     encoder0 = Encoder(dut.clk, dut.enc0_a, dut.enc0_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
     encoder1 = Encoder(dut.clk, dut.enc1_a, dut.enc1_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
     encoder2 = Encoder(dut.clk, dut.enc2_a, dut.enc2_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
@@ -37,17 +49,10 @@ async def test_all(dut):
     assert dut.pwm1_out == 0
     assert dut.pwm1_out == 0
 
-    # do 3 ramps for each encoder up to a smaller amount as the test with clock divider is much slower
-    max_count = 255
-    for encoder, dut_enc in zip([encoder0, encoder1, encoder2], [dut.enc0, dut.enc1, dut.enc2]):
-        for i in range(clocks_per_phase * 2 * max_count):
-            await encoder.update(1)
-
-        # let noisy transition finish, otherwise can get an extra count
-        for i in range(10):
-            await encoder.update(0)
-        
-        assert(dut_enc == max_count)
+    # do 3 ramps for each encoder 
+    await run_encoder_test(encoder0, dut.enc0)
+    await run_encoder_test(encoder1, dut.enc1)
+    await run_encoder_test(encoder2, dut.enc2)
 
     # sync to pwm
     await RisingEdge(dut.pwm0_out)
