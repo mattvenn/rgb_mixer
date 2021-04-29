4f25368b5146bd01391b38d62476bed16ae35ef3
James Clark <james@jamesclark.dev>
2021-04-29 01:24:12 +0100

    Add debounce

--- src/debounce.v
+++ src/debounce.v
@@ -1,10 +1,29 @@
 `default_nettype none
 `timescale 1ns/1ns
-module debounce (
+module debounce #(
+    parameter HISTORY = 8) (
     input wire clk,
     input wire reset,
     input wire button,
     output reg debounced
 );
 
+   reg [HISTORY-1:0] shift;
+   reg out;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            shift <= 0;
+            out <= 0;
+        end else begin
+            shift <= {shift[HISTORY-2:0], button};
+            
+            if (shift == {HISTORY{1'b1}}) begin
+                debounced <= 1'b1;
+            end
+            if (shift == {HISTORY{1'b0}}) begin
+                debounced <= 0;
+            end
+        end
+    end
 endmodule
