3316c91697826f3f95a75d03fd592aadea3ef8f5
James Clark <james@jamesclark.dev>
2021-04-29 01:24:01 +0100

    Add PWM

--- src/pwm.v
+++ src/pwm.v
@@ -1,10 +1,26 @@
 `default_nettype none
 `timescale 1ns/1ns
-module pwm (
+module pwm #(
+    parameter WIDTH = 8,
+    parameter INVERT = 0) (
     input wire clk,
     input wire reset,
     output wire out,
-    input wire [7:0] level
+    input wire [WIDTH-1:0] level
     );
 
+    reg [WIDTH-1:0] counter;
+
+    always @(posedge clk) begin
+        if (reset) begin
+            counter <= 0;
+        end else begin
+            counter <= counter + 1'b1;
+        end
+    end
+    if (INVERT) begin
+        assign out = !(counter < level);
+    end else begin
+        assign out = counter < level;
+    end
 endmodule
