eadd7d8fc26f6d1039eba7f56728e65999871d6f
James Clark <james@jamesclark.dev>
2021-05-03 01:48:05 +0100

    Add generator RGB mixer

--- src/rgb_mixer.v
+++ src/rgb_mixer.v
@@ -1,17 +1,23 @@
 `default_nettype none
 `timescale 1ns/1ns
-module rgb_mixer (
+module rgb_mixer #(
+    parameter NUM_LEDS = 3)(
     input clk,
     input reset,
-    input enc0_a,
-    input enc0_b,
-    input enc1_a,
-    input enc1_b,
-    input enc2_a,
-    input enc2_b,
-    output pwm0_out,
-    output pwm1_out,
-    output pwm2_out
+    input [NUM_LEDS-1:0] enc_a,
+    input [NUM_LEDS-1:0] enc_b,
+    output [NUM_LEDS-1:0] pwm_out
 );
+    wire [NUM_LEDS-1:0][7:0] enc;
+    wire [NUM_LEDS-1:0] enc_a_debounced, enc_b_debounced;
 
+    genvar i;
+    generate
+        for (i = 0; i < NUM_LEDS; i = i + 1) begin : encoder
+            debounce debounce_a (.clk(clk), .reset(reset), .button(enc_a[i]), .debounced(enc_a_debounced[i]));
+            debounce debounce_b (.clk(clk), .reset(reset), .button(enc_b[i]), .debounced(enc_b_debounced[i]));
+            encoder encoder     (.clk(clk), .reset(reset), .a(enc_a_debounced[i]), .b(enc_b_debounced[i]), .value(enc[i]));
+            pwm pwm             (.clk(clk), .reset(reset), .level(enc[i]), .out(pwm_out[i]));
+        end
+    endgenerate
 endmodule
--- test/test_rgb_mixer.py
+++ test/test_rgb_mixer.py
@@ -5,14 +5,12 @@ import random
 from test.test_encoder import Encoder
 
 clocks_per_phase = 10
+num_encoders = 3
 
 async def reset(dut):
-    dut.enc0_a <= 0
-    dut.enc0_b <= 0
-    dut.enc1_a <= 0
-    dut.enc1_b <= 0
-    dut.enc2_a <= 0
-    dut.enc2_b <= 0
+    for i in range(num_encoders):
+        dut.enc_a[i] <= 0
+        dut.enc_b[i] <= 0
     dut.reset  <= 1
 
     await ClockCycles(dut.clk, 5)
@@ -32,33 +30,30 @@ async def run_encoder_test(encoder, dut_enc, max_count):
 @cocotb.test()
 async def test_all(dut):
     clock = Clock(dut.clk, 10, units="us")
-    encoder0 = Encoder(dut.clk, dut.enc0_a, dut.enc0_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
-    encoder1 = Encoder(dut.clk, dut.enc1_a, dut.enc1_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
-    encoder2 = Encoder(dut.clk, dut.enc2_a, dut.enc2_b, clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4)
+    encoders = []
+    for i in range(num_encoders):
+        encoders.append(Encoder(dut.clk, dut.enc_a[0], dut.enc_b[0], clocks_per_phase = clocks_per_phase, noise_cycles = clocks_per_phase / 4))
 
     cocotb.fork(clock.start())
 
     await reset(dut)
-    assert dut.enc0 == 0
-    assert dut.enc1 == 0
-    assert dut.enc2 == 0
+    for i in range(num_encoders):
+        assert dut.enc[i] == 0
 
     # pwm should all be low at start
-    assert dut.pwm0_out == 0
-    assert dut.pwm1_out == 0
-    assert dut.pwm1_out == 0
+    for i in range(num_encoders):
+        assert dut.pwm_out[i] == 0
 
     # do 3 ramps for each encoder 
     max_count = 255
-    await run_encoder_test(encoder0, dut.enc0, max_count)
-    await run_encoder_test(encoder1, dut.enc1, max_count)
-    await run_encoder_test(encoder2, dut.enc2, max_count)
+    for i in range(num_encoders):
+        await run_encoder_test(encoders[i], dut.enc[i], max_count)
 
     # sync to pwm
-    await RisingEdge(dut.pwm0_out)
+
+    await RisingEdge(dut.pwm_out[0])
     # pwm should all be on for max_count 
-    for i in range(max_count): 
-        assert dut.pwm0_out == 1
-        assert dut.pwm1_out == 1
-        assert dut.pwm2_out == 1
+    for i in range(max_count):
+        for i in range(num_encoders):
+            assert dut.pwm_out[i] == 1
         await ClockCycles(dut.clk, 1)
