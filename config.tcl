9f1ae2b05210bc8e5dbf0622e8c533d0db36a4c7
Matt Venn <matt@mattvenn.net>
2022-11-14 12:57:35 +0100

    change config for mpw7 tagged tools

--- config.tcl
+++ config.tcl
@@ -7,6 +7,10 @@ set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 set ::env(PL_TARGET_DENSITY) 0.4
 set ::env(FP_CORE_UTIL) 30
 
+# design is so small, PDN will fail without manually increasing it
+set ::env(FP_SIZING) absolute
+set ::env(DIE_AREA) "0 0 150 150"
+
 # Fill this
 set ::env(CLOCK_PERIOD) "10"
 set ::env(CLOCK_PORT) "clk"
